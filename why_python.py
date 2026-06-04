4fe549c2eae3d400b867b801ec4cf7f702711e25
sangam962895 <146249520+sangam962895@users.noreply.github.com>
2026-06-04 14:00:29 +0530

    Added Python interview notes

---
+++ why_python.py
@@ -0,0 +1,105 @@
+# python is high level Interpreted language that human give instruction to machine to perform some task.
+
+#High_level: 1. close to human language
+ #           2. easy to learn and written(no tension of (0&1))
+
+# Interpreted: Executes code line by line
+#             -Easier debugging because errors are shown at the line where they occur
+
+# Syntax: mean a set of rules to write code in python, ex. jaise english  me grammer hota hai vaise hi python  me syntax hota hai.
+
+# indention : indentation is define block of code,ye batata hai ki kon sa code kis block me hai
+
+            #- Python uses indentation instead of {}
+
+# comment: Python Interpreter comments ko execute nahi karta.
+#             Ignored by Python Interpreter
+#         - Used for explanation and documentation
+
+# ariables:
+#    - Used to store data in memory
+
+#    Example:
+#    name = "Sangam"
+#    age = 22
+
+# Memory: 
+
+# Data Types
+#    - int      -> 10
+#    - float    -> 10.5
+#    - str      -> "Hello"
+#    - bool     -> True/False
+#    - list     -> [1,2,3]
+#    - tuple    -> (1,2,3)
+#    - set      -> {1,2,3}
+#    - dict     -> {"name":"Sangam"}
+
+
+# Operators
+#    - Arithmetic (+,-,*,/,%,**,//)
+#    - Comparison (==, !=, >, <)
+#    - Logical (and, or, not)
+#    - Assignment (=, +=, -=)
+
+
+# Type Casting
+#     - Converting one data type to another
+
+#     int("10")
+#     str(10)
+#     float(10)
+
+
+# input and output: Input is used to take data from the user, while output is used to display information to the user.
+
+# Conditional Statements: Decision making statements.
+
+# Why use them?
+
+# Jab different conditions ke basis par different actions perform karne hon.
+
+
+# Loops: Same task multiple times perform karne ke liye.
+#     For Loop:
+#         Known number of iterations.
+#     While Loop: 
+#         Condition true hone tak chalta hai.
+
+# Loops are used to execute a block of code repeatedly until a condition is met.
+
+# What is a Function?
+#     A function is a reusable block of code that performs a specific task.
+#     Specific task = Ek particular kaam jo function ko karne ke liye banaya gaya hai.
+# ex. 
+
+# Arguments vs Parameters
+# Parameters are variables defined in a function definition, while arguments are actual values passed during function calls.
+
+# What is Return?
+# Function se value wapas bhejne ke liye.
+# Why use it?
+# Result ko function ke bahar use karne ke liye.
+# Interview Answer
+# The return statement sends a value back to the caller and terminates the function execution.
+
+# Scope:
+#     Scope defines the visibility and accessibility of variables within a program.
+
+# List Comprehension: 
+#                     List comprehension provides a concise way to create lists in a single line.
+# Why use it?
+#         Less code and better readability.
+
+# Exception Handling:
+#                     Runtime error.
+#                     Program crash hone se bachane ke liye.
+#                     Exception handling is used to handle runtime errors gracefully without terminating the program.
+
+
+# Modules:> 
+#             A module is a Python file containing reusable code.
+#             Python file containing functions, classes, and variables.
+
+# Packages:
+#             A package is a collection of related Python modules organized in directories.
